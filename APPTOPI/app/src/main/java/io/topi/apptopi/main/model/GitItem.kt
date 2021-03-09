cedbe3fdd72e2120e9ee63f0d3b0f05d15ab43d5
andrefellype03 <andre.fellype@topi.io>
2021-03-09 12:07:14 -0300

    TREINAMENTO DE KOTLIN

--- APPTOPI/app/src/main/java/io/topi/apptopi/main/model/GitItem.kt
+++
@@ -1,6 +0,0 @@
-package io.topi.apptopi.main.model
-
-import java.io.Serializable
-
-data class GitItem(var id: Int, var name: String, var description: String, var stargazers_count: Int,
-              var forks: Int, var owner: Owner): Serializable {}
--- APPTOPI/app/src/main/java/io/topi/apptopi/main/model/Owner.kt
+++
@@ -1,5 +0,0 @@
-package io.topi.apptopi.main.model
-
-import java.io.Serializable
-
-data class Owner(var id: Int, var avatar_url: String, var login: String): Serializable
